a8dbeb1d82cf735a5bbed4d2c00bb5c6032d50ed
Graeme Coupar <grambo@grambo.me.uk>
2013-12-26 17:37:59 +0000

    Moved git functionality into own namespace

---
+++ src/stubbly/git.clj
@@ -0,0 +1,107 @@
+(ns stubbly.git
+  (:use [clj-jgit.porcelain :only [load-repo]]
+        [clj-jgit.querying :only [changed-files rev-list]]
+        [clojure.core.match :only [match]])
+  (:import [java.io ByteArrayOutputStream]
+           [org.eclipse.jgit.diff DiffFormatter RawTextComparator]
+           [org.eclipse.jgit.api Git]
+           [org.eclipse.jgit.revwalk RevCommit])
+  (:require [clojure.string]
+            [stubbly.levenshtein :as levenshtien]))
+
+(declare diff-for-commit diff-split-with)
+
+(def repo-path "/Users/grambo/src/rolepoint-app")
+
+(def repo (load-repo repo-path))
+
+(def revisions (rev-list repo))
+
+(def revision (first revisions))
+
+(changed-files repo revision)
+
+(def header-regexp #"^diff --git a/(.*) b/(.*)$")
+(def section-header-regexp #"^@@ -(\d+),(\d+) \+(\d+),(\d+) @@$")
+
+(defn- partition-by-re
+  [regexp input]
+  (partition-by #(re-matches regexp %) input))
+
+(defn split-diff
+  [input]
+  (->> input
+       clojure.string/split-lines
+       (partition-by-re header-regexp)
+       (partition 2)
+       (map flatten)))
+
+(defn- diff-for-commit
+  [^Git repo ^RevCommit rev-commit]
+  (if-let [parent (first (.getParents rev-commit))]
+    (let [stream (ByteArrayOutputStream.)]
+      (doto
+        (DiffFormatter. stream)
+        (.setRepository (.getRepository repo))
+        (.setDiffComparator RawTextComparator/DEFAULT)
+        (.setDetectRenames false)
+        (.format parent rev-commit))
+      (.toString stream))
+    ; TODO: Write the else branch of if-let (for the first commit)
+    ))
+
+(def split-diffs (split-diff (diff-for-commit repo revision)))
+
+(defn split-diff-sections
+  [input]
+  (->> input
+       (partition-by-re section-header-regexp)
+       (drop 1)
+       (partition 2)
+       (flatten)))
+
+(split-diff-sections (second split-diffs))
+(map split-diff-sections split-diffs)
+
+(defn- parse-line
+  [[first-char & rest-line]]
+  (let [kind (match [first-char]
+                    [\+]  :added
+                    [\-]  :removed
+                    :else :context)]
+    {:contents (if (= kind :context)
+                 (str first-char (apply str rest-line))
+                 (apply str rest-line))
+     :kind kind}))
+
+(defn- remove-kind [kind lines]
+  (remove #(= kind (:kind %)) lines))
+
+(defn parse-section
+  "Parses a section into a list of changed lines"
+  [[section-header & diff-lines]]
+  (let [[_ a-start a-len b-start b-len] (re-matches section-header-regexp
+                                                    section-header)
+        lines (map parse-line diff-lines)
+        [added removed] ((juxt #(remove-kind :removed %)
+                              #(remove-kind :added %))
+                         lines)
+        linenum-fn (fn [lines start-index]
+                     (map-indexed
+                      #(assoc %2 :line-num (+ %1 (Integer. start-index)))
+                      lines))]
+    (set (remove #(= :context (:kind %))
+      (concat (linenum-fn added b-start)
+              (linenum-fn removed a-start))))))
+
+(def section (split-diff-sections (first split-diffs)))
+
+(parse-section section)
+(defn parse-file-diff
+  "Parses a single diff for a file into a description hash"
+  [[header-line & other-lines]]
+  (let [sections (split-diff-sections other-lines)]
+    {:file (second (re-matches header-regexp header-line))
+     :lines (parse-section sections)}))
+
+(parse-file-diff (first split-diffs))
---
+++ test/stubbly/git_test.clj
@@ -0,0 +1,46 @@
+(ns stubbly.git-test
+  (:use midje.sweet
+        stubbly.git)
+  (:require [clojure.string]))
+
+(def ^:private test-diff
+  "diff --git a/project.clj b/project.clj
+index ca84a0c..19b1a3b 100644
+--- a/project.clj
++++ b/project.clj
+@@ -6,4 +6,5 @@
+   :dependencies [[org.clojure/clojure \"1.5.1\"]
+                  [clj-jgit \"0.6.4\"]
+                  [clojurewerkz/neocons \"2.0.0\"]]
++  :profiles {:dev {:dependencies [[midje \"1.5.1\"]]}}
+   :main stubbly.core)
+diff --git a/test/stubbly/core_test.clj b/test/stubbly/core_test.clj
+index d75c820..6297bd9 100644
+--- a/test/stubbly/core_test.clj
++++ b/test/stubbly/core_test.clj
+@@ -1,5 +1,5 @@
+ (ns stubbly.core-test
+-  (:use clojure.test
++  (:use midje.sweet
+         stubbly.core))
+
+ (deftest a-test")
+
+(def ^:private test-diff-lines
+  (clojure.string/split-lines test-diff))
+
+(fact "split-diff splits diffs into seqs of (header, lines)"
+  (split-diff test-diff) => (split-at 10 test-diff-lines))
+
+(fact "split-diff-sections splits out a seq of sections"
+  (split-diff-sections (take 10 test-diff-lines))
+      => (drop 4 (take 10 test-diff-lines)))
+
+(fact "parse-section returns added & removed lines with indexes"
+  (parse-section (drop 14 test-diff-lines))
+      => #{{:kind :removed
+            :line-num 2
+            :contents "  (:use clojure.test"}
+           {:kind :added
+            :line-num 2
+            :contents "  (:use midje.sweet"}})
