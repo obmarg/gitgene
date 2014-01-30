e47832e36c21ecfcf46415db7d41ce11f541fba6
Graeme Coupar <grambo@grambo.me.uk>
2014-01-30 23:40:51 +0000

    Om based table in cljs

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -7,4 +7,23 @@
   (om/component
    (dom/div nil "Hello world")))
 
-(om/root {} widget (.getElementById js/document "app"))
+(defn
+  table-row [columns]
+  (om/component
+   (apply dom/tr nil
+    (map #(dom/td nil %) columns))))
+
+(defn
+  table [data]
+  (om/component
+   (dom/table #js {:className "table table-striped"}
+     (dom/thead nil
+      (apply dom/tr nil
+       (map #(dom/th nil %) (:headers data))))
+     (apply dom/tbody nil
+      (om/build-all table-row (:rows data))))))
+
+(def app-state (atom {:headers ["test" "test 2"]
+                      :rows [["1" "2"] ["3" "4"]]}))
+
+(om/root app-state table (.getElementById js/document "app"))
