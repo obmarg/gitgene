0e2ce7e011c56d80f27da159c5da2d7cdde26132
Graeme Coupar <grambo@grambo.me.uk>
2014-01-30 22:39:41 +0000

    Worked on initial layout some more

--- src/clj/gitgene/views.clj
+++ src/clj/gitgene/views.clj
@@ -24,20 +24,28 @@
       "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css")]
     [:body
      [:nav {:class "navbar navbar-inverse navbar-fixed-top" :role "navigation"}
-      [:div {:class "container"}
+      [:div {:class "container-fluid"}
        [:div {:class "navbar-header"}
-        [:a {:class "navbar-brand" :href "#"} "GitGene"]]]]
-     [:div {:class "container" :style "margin-top: 60px"}
+        [:a {:class "navbar-brand" :href "#"}
+         [:i {:class "fa fa-flask"}] " GitGene"]]]]
+     [:div {:class "container-fluid" :style "margin-top: 60px"}
       [:div {:class "row"}
-       [:div {:class "col-xs-3"}
-        [:h4 "View Stats"]
-        [:ul {:class "nav nav-pills nav-stacked"}
+       [:div {:class "col-xs-9 col-xs-offset-3"}
+        [:ul {:class "nav nav-pills"}
          (active-menu-item "By Person" "fa fa-user" "#")
          (menu-item "In A File" "fa fa-file" "#")
          (menu-item "In A Commit" "fa fa-files-o" "#")
-         ]]
-       [:div {:class "col-xs-9"}
-        [:div {:id "app"}]]]]]
+         ]]]
+     [:div {:class "row"}
+      [:div {:class "col-xs-3"}
+       [:ul {:class "nav nav-pills nav-stacked"}
+        (active-menu-item "Number Of Changes" "fa fa-exchange" "#")
+        (menu-item "Number Of Additions" "fa fa-plus" "#")
+        (menu-item "Number Of Deletions" "fa fa-minus" "#")
+        (menu-item "Total Churn" "fa fa-refresh" "#")
+        ]]
+      [:div {:class "col-xs-9"}
+       [:div {:id "app" :style "padding-top: 15px"}]]]]]
      (include-js
        "http://fb.me/react-0.8.0.js"
        "out/goog/base.js"
